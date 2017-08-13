e29027874283f752fe94236b81499351fba4d506
Maxim Dužij <maxim.duzij@riganti.cz>
2017-08-13 20:57:05 +0200

    Initial commit

---
+++ localizationTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using localizationTest.Resourses;
+using System;
+using System.Globalization;
+using System.Resources;
+
+namespace localizationTest
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AppResource.resourseText);
+            Console.ReadLine();
+            CultureInfo.CurrentCulture = new CultureInfo("cs");
+            CultureInfo.CurrentUICulture = new CultureInfo("cs");
+            CultureInfo cul = new CultureInfo("cs");
+
+            Console.WriteLine(AppResource.resourseText);
+            Console.WriteLine(AppResource.ResourceManager.GetString("resourseText"));
+            Console.WriteLine(AppResource.ResourceManager.GetString("resourseText", cul));
+            Console.ReadLine();
+        }
+    }
+}
